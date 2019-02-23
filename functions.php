2decb93b8c505e9e7c996cd8828f2983a90b1853
Victor Comstock <vitor1correia@gmail.com>
2019-02-23 19:19:02 -0300

    #2 Creating the essential wp Stuff

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+function script_enqueuer(){
+
+
+}
+
+add_action(,'script_enqueuer');
+
+function style_enqueuer(){
+
+}
+add_action(,'style_enqueuer');
+ ?>
---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+  <head>
+    <?php wp_head();?>
+    <meta charset="utf-8">
+    <title><?php wp_title();  ?></title>
+  </head>
+  <body>
