2c6ffa3293a866df54269982eac562ae6aceed73
Victor Comstock <vitor1correia@gmail.com>
2019-03-19 22:32:05 -0300

    more and more

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function assets_enqueuer(){
   wp_enqueue_style('Bootstrap css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
   wp_enqueue_script('Bootstrap JS', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js');
   wp_enqueue_script('Bootstrap Bundle JS', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js');
+  wp_enqueue_style('Font Awesome CSS', 'https://use.fontawesome.com/releases/v5.7.2/css/all.css');
 }
 add_action( 'wp_enqueue_scripts','assets_enqueuer');
 
