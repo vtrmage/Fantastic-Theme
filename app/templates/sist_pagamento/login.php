927b674cb135a12cdfecab7120840f038a645975
Victor Comstock <vitor1correia@gmail.com>
2019-03-24 21:25:07 -0300

    GULP PERFEITO

--- app/templates/sist_pagamento/login.php
+++ app/templates/sist_pagamento/login.php
@@ -1,12 +1,61 @@
-<?php get_header(); ?>
+<?php get_header();
 
-<h1>home page do sistema de sist_pagamento</h1>
+if(isset($_POST['submit'])){
 
-<label for="login"> login</label>
-<input type="text" id='login'></input>
-<label for="senha"> Senha</label>
-<input type="password" id='senha'></input>
-<button type="button" class="btn btn-primary"> CLERO</button>
+$username =  $_POST['username'];
+$password =  $_POST['password'];
+// CONNECTING TO THE DATABASE, ADDRES, USER, PASSWORD, DATABASE NAME
+$connection = mysqli_connect('localhost','root','root','sistema_pagamento');
 
+
+
+   //$cria_user = mysqli_query ($connection, "insert into usuario (login, senha) values ('$username', '$password');");
+             //INSERT INTO usuario (login, senha) VALUES ( 'jiren', 'vera');
+   //$cria_user = mysqli_query(, $query);
+   $ver_usuario = mysqli_query ($connection, "SELECT * FROM usuario WHERE login='$username' and senha='$password'");
+
+  if($ver_usuario){
+    echo "TEM USUARIO MAN";
+
+    print_r($ver_usuario);
+  }
+  else{
+    echo "NAO TEM USUARIO MAN";
+  }
+
+
+// if ($username && $password) {
+//
+//   echo $username;
+//   echo $password;
+// }
+//
+// else {
+//   echo 'tem nada nao man';
+// }
+
+
+
+};
+
+?>
+
+<div class="container">
+<h1>Tela de login</h1>
+
+<form action="<?php get_theme_file_uri('app/templates/sist_pagamento/login.php')?>" method="post">
+  <div class="form-group">
+    <label for="username">Email address</label>
+    <input type="text" class="form-control" name="username" id="username" aria-describedby="emailHelp" placeholder="Enter usernaem">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
+  </div>
+  <button type="submit" class="btn btn-warning" name="submit" value="Submit">Submit</button>
+</form>
+
+</div>
 
 <?php get_footer(); ?>
--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,48 +1,24 @@
-var gulp       = require('gulp'),
-    livereload = require('gulp-livereload'),
-    uglify     = require('gulp-uglify');
-
-  
-
-  const sass       = require('gulp-sass');
-const rename       = require("gulp-rename");
-const sourcemaps   = require('gulp-sourcemaps');
-const path         = {
-  sass: '../sass/app.scss'
-};
-const sassOptions = {
-    errLogToConsole: true,
-    outputStyle: 'compressed'
-};
-
-gulp.task('minify', async function(){
-  gulp.src('../app/modules/Listicles/*.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('../minifications/'));
-});
+const gulp        = require ('gulp');
+const sass        = require ('gulp-sass');
+const browserSync = require ('browser-sync').create();
 
 
-gulp.task('sass_minify', function() {
+function compile_sass(){
+  // Aqui estou  escolhendo o arquivo de origem
+   return gulp.src('../sass/app.scss')
+   // Para mostrar os erros de uma forma mais amigavel
+   .pipe(sass().on('error', sass.logError))
+   .pipe(gulp.dest('../minifications'))
+   .pipe(browserSync.stream());
+}
 
-    return gulp.src(path.sass)
-        .pipe(sourcemaps.init())
-        .pipe(sass(sassOptions).on('error', sass.logError))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('../minifications'))
+function watch(){
+  browserSync.init({
+    proxy:"http://localhost:8888/project/"
+  });
+  gulp.watch('../sass/**/*.scss', gulp.parallel('compile_sass'));
 
-});
-
-
-gulp.task('watch', ['sass_minify'], function(){
-   gulp.watch(src.path, ['sass_minify']);
-});
-
-gulp.task('watch', ['sass_minify'], function() {
-
-    gulp.watch(src.watchCSS, ['sass']);
-    //gulp.watch(src.watchJS, ['scripts']);
-});
-
-gulp.task('default', async function(){
-  console.log('jira com a jiromba do jiren');
-});
+  gulp.watch('../app/**/*.php').on('change', browserSync.reload);
+}
+exports.compile_sass = compile_sass;
+exports.watch        = watch;
